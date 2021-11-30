2035cb5f53950c25c65a9d638cf4f9746eaa8bb0
Ryan Cole <le.mimikyu52@gmail.com>
2021-11-30 15:29:47 -0500

    added PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float playerSpeed = 5;
+
+    public bool mouseMove = true;
+    public Vector3 mousePos;
+//------------------------------------------------
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        LocatePointer();
+        Move();
     }
+
+    public void LocatePointer()
+    {
+        mousePos = Input.mousePosition;
+
+    }
+
+    public void Move()
+    {
+        Vector3 offset = new Vector3(-3, -3, 0);
+
+        if (mouseMove)
+        {
+            Vector3 playerPos = mousePos + offset;
+            transform.position = playerPos * playerSpeed * Time.deltaTime;
+
+        }
+    }
+
 }
